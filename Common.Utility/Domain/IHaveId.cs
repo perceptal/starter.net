845a5844f3bcbc48b453c30a57894dd3e0c21626
Johnny <johnny@recipher.co.uk>
2011-06-21 22:26:25 +0100

    Added some common domain classes

---
+++ Common.Utility/Domain/IHaveId.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Common.Domain
+{
+    public interface IHaveId
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Common.Utility/Domain/ValueObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Domain
+{
+    public abstract class ValueObject<T> : IEquatable<T> where T : ValueObject<T>
+    {
+        protected abstract IEnumerable<object> Reflect();
+
+        public static bool operator ==(ValueObject<T> x, ValueObject<T> y)
+        {
+            if (Equals(x, null) ^ Equals(y, null))
+            {
+                return false;
+            }
+
+            return Equals(x, null) || x.Equals(y);
+        }
+
+        public static bool operator !=(ValueObject<T> x, ValueObject<T> y)
+        {
+            return !(x == y);
+        }
+
+        public override bool Equals(object o)
+        {
+            if (ReferenceEquals(null, o))
+            {
+                return false;
+            }
+
+            return o.GetType() == GetType() && Equals(o as T);
+        }
+
+        public override int GetHashCode()
+        {
+            int seed = 36;
+
+            int hc = Reflect()
+                .Aggregate(
+                    seed,
+                    (hashCode, value) =>
+                        value == null
+                            ? hashCode
+                            : CalculateHashCode(hashCode, value, ++seed));
+
+            return hc;
+        }
+
+        private int CalculateHashCode(int hc, object value, int multiplier)
+        {
+            return (hc ^ value.GetHashCode()) * multiplier;
+        }
+
+        public virtual bool Equals(T other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            return ReferenceEquals(this, other) || this.ValuesAreEqual(other);
+        }
+
+        /// <summary>
+        /// Uses the Reflect Method to determine whether values are equal - you can override if this is not the behaviour you require
+        /// </summary>
+        protected virtual bool ValuesAreEqual(T other)
+        {
+            return Reflect().SequenceEqual(other.Reflect());
+        }
+
+        public override string ToString()
+        {
+            return "{ " + (string)Reflect().Aggregate((l, r) => l + ", " + r) + " }";
+        }
+    }
+}
